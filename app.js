e329a34129463eb03891b3bc74906665504e70ff
Cole DeVore <c16devore@gmail.com>
2022-08-01 18:05:58 -0700

    removed the random aspect from the first (only at the moment) switch case i plan on maybe implementing it in a different way naybe tied ti an event listener on the 'w' key if possible

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ function randoNum(min, max) {
     return Math.random() * (max-min) +min;
 }
 function enemySpawn() {
-    randoNum(1, 16)
-    if (randoNum <= 5) {
+    randoNum(1, 4)
+    if (randoNum === 1) {
         let giantRat = {
             health: 10, 
-            name: "Giant at"
+            name: "Giant Rat"
 
         }
-    } else if (randoNum <= 10 ) {
+    } else if (randoNum === 2) {
         var skeleton = {
             health: 15,
             name: "Skeleton"
         }
-    }else if (randoNum <=15) {
+    }else if (randoNum === 3) {
         var prisonGuard = {
             health:20,
             name: "Prison Guard"
@@ -32,18 +32,14 @@ var playerInv = []
     var nameSave = nameQ
 
     const prompt = readline.keyIn("_-_-_-_-_- well "+ nameSave + " you awake to find yourslef in a dark room in what semingly is a dungeon. you find your chains unlocked, a club on the floor beside you, and the door to your cell open you are free to leave...or so it seems. press 'e' to grab the club and/or Press 'w' to walk throught the door ")
-    switch (prompt, prompt1) {
+    switch (prompt) {
         case 'w': 
-        if (randoNum(1, 5) < 3) {
-            
-        } else {
             var prompt1 = readline.keyIn("youe exit the cell and see a long hallway stretch out i front of you with doors lining the sides all are boarded up seems the only thing to do is keep going. press 'w' to keep walking")
-        }
         break 
         case 'e':
             var club = "club"
             playerInv.push(club)
-            const prompt2 = readline.keyIn("you pick the club up off the ground feeling a little better now that your armed there dosent appear to be anything else in the cell with you only he open door is of any interest. press 'w' to walk through the door" )
+            const prompt2 = readline.keyIn("you pick the club up off the ground feeling a little better now that your armed there dosent appear to be anything else in the cell with you only the open door is of any interest. press 'w' to walk through the door" )
             break 
     }
 // }
